66f4ff448a4848c3c5232b3e62f4d631ba46732e
Sarfaraz Nawaz <sarfaraz.nawaz@tenx.tech>
2020-06-21 20:59:11 +0530

    Change Inherit Line

--- default.nix
+++ default.nix
@@ -14,8 +14,9 @@
 let
   # 1. Setup nixpkgs with nixpkgs-mozilla overlay and cargo2nix overlay.
   packages = import nixpkgs {
-    inherit system crossSystem;
     overlays =
+    inherit system;
+    inherit crossSystem;
       let
         rustOverlay = import "${nixpkgsMozilla}/rust-overlay.nix";
         cargo2nixOverlay = import ./overlay;
